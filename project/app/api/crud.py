9588efd20c5e5cd926a5e4afbadd38e2e442c88e
jordanhoare <jordanhoare0@gmail.com>
2022-03-29 21:18:59 +1000

    test_read_all_summaries

--- project/app/api/crud.py
+++ project/app/api/crud.py
@@ -1,3 +1,5 @@
+from typing import List, Union
+
 from app.models.pydantic import SummaryPayloadSchema
 from app.models.tortoise import TextSummary
 
@@ -14,3 +16,15 @@ async def post(payload: SummaryPayloadSchema) -> int:
     )
     await summary.save()
     return summary.id
+
+
+async def get(id: int) -> Union[dict, None]:
+    summary = await TextSummary.filter(id=id).first().values()
+    if summary:
+        return summary
+    return None
+
+
+async def get_all() -> List:
+    summaries = await TextSummary.all().values()
+    return summaries
