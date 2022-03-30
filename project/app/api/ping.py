6711e962b941d82fad1ae1e514b19c4dc9f7fa92
jordanhoare <jordanhoare0@gmail.com>
2022-03-30 10:10:15 +1000

    heroku CD test

--- project/app/api/ping.py
+++ project/app/api/ping.py
@@ -12,7 +12,7 @@ async def pong(settings: Settings = Depends(get_settings)):
     settings: The value returned (Settings) is assigned for the return statements
     """
     return {
-        "ping": "pong!",
+        "ping": "pong",
         "environment": settings.environment,
         "testing": settings.testing,
     }
