fcb01ff1fa6b5f48c1049cf95bb95af09b982f3c
jordanhoare <jordanhoare0@gmail.com>
2022-04-19 11:42:31 +1000

    init jinja Atlantis template

--- project/app/api/crud.py
+++ project/app/api/crud.py
@@ -1,44 +1,43 @@
 from typing import List, Union
 
 from app.models.pydantic import SummaryPayloadSchema
-from app.models.tortoise import Stock_Pydantic
+from app.models.tortoise import TextSummary
 from app.summariser import generate_summary
 
 
 async def get(id: int) -> Union[dict, None]:
-    summary = await Stock_Pydantic.filter(id=id).first().values()
+    summary = await TextSummary.filter(id=id).first().values()
     if summary:
         return summary
     return None
 
 
 async def get_all() -> List:
-    summaries = await Stock_Pydantic.all().values()
+    summaries = await TextSummary.all().values()
     return summaries
 
 
 async def delete(id: int) -> int:
-    summary = await Stock_Pydantic.filter(id=id).first().delete()
+    summary = await TextSummary.filter(id=id).first().delete()
     return summary
 
 
 async def put(id: int, payload: SummaryPayloadSchema) -> Union[dict, None]:
-    summary = await Stock_Pydantic.filter(id=id).update(
+    summary = await TextSummary.filter(id=id).update(
         url=payload.url, summary=payload.summary
     )
     if summary:
-        updated_summary = await Stock_Pydantic.filter(id=id).first().values()
+        updated_summary = await TextSummary.filter(id=id).first().values()
         return updated_summary
     return None
 
 
 async def post(payload: SummaryPayloadSchema) -> int:
-    """
-    A utility function to create new summaries
+    """A utility function to create new summaries
     - takes a payload object
-    - creates a new Stock_Pydantic instance
+    - creates a new TextSummary instance
     - returns the generated ID
     """
-    summary = Stock_Pydantic(url=payload.url, summary="")
+    summary = TextSummary(url=payload.url, summary="")
     await summary.save()
     return summary.id
--- project/app/models/tortoise.py
+++ project/app/models/tortoise.py
@@ -1,65 +1,53 @@
-# from fileinput import close
-
-# from tortoise import fields, models
-# from tortoise.contrib.pydantic import pydantic_model_creator
-
-from tortoise import fields, init_models, models
+from tortoise import fields, models
 from tortoise.contrib.pydantic import pydantic_model_creator
 
 
-class Stock(models.Model):
+class TextSummary(models.Model):
     """
-    This references a Stock
-    """
-
-    id = fields.IntField(pk=True)
-    symbol = fields.CharField(max_length=20, unique=True, null=False)
-    company = fields.CharField(max_length=50, null=False)
-    stock_prices: fields.ReverseRelation["Stock_Price"]
-
-
-class Stock_Price(models.Model):
-    """
-    This references a Stock_Price in a Stock
+    Define a new database model called TextSummary.
     """
 
-    id = fields.IntField(pk=True)
-    stock_id = fields.IntField(null=False)
-    date = fields.DatetimeField(null=False)
-    open = fields.FloatField(null=False)
-    high = fields.FloatField(null=False)
-    open = fields.FloatField(null=False)
-    close = fields.FloatField(null=False)
-    adjusted_close = fields.FloatField(null=False)
-    volume = fields.IntField(null=False)
+    url = fields.TextField()
+    summary = fields.TextField()
+    created_at = fields.DatetimeField(auto_now_add=True)
 
-    stock = fields.ForeignKeyField(
-        "models.Stock",
-        related_name="stock_prices",
-        description="The stock this describe",
-    )
+    def __str__(self):
+        return self.url
 
 
-# Tortoise.init_models(["__main__"], "models")
-Stock_Pydantic = pydantic_model_creator(Stock)
-Stock_Price_Pydantic = pydantic_model_creator(Stock_Price)
-
-
-# from tortoise import fields, models
-# from tortoise.contrib.pydantic import pydantic_model_creator
-
-
-# class TextSummary(models.Model):
+# class Stock(models.Model):
 #     """
-#     Define a new database model called TextSummary.
+#     This references a Stock
 #     """
 
-#     url = fields.TextField()
-#     summary = fields.TextField()
-#     created_at = fields.DatetimeField(auto_now_add=True)
+#     id = fields.IntField(pk=True)
+#     symbol = fields.CharField(max_length=20, unique=True, null=False)
+#     company = fields.CharField(max_length=50, null=False)
+#     stock_prices: fields.ReverseRelation["Stock_Price"]
 
-#     def __str__(self):
-#         return self.url
 
-# Tortoise.init_models(["__main__"], "models")
-# SummarySchema = pydantic_model_creator(TextSummary)
+# class Stock_Price(models.Model):
+#     """
+#     This references a Stock_Price in a Stock
+#     """
+
+#     id = fields.IntField(pk=True)
+#     stock_id = fields.IntField(null=False)
+#     date = fields.DatetimeField(null=False)
+#     open = fields.FloatField(null=False)
+#     high = fields.FloatField(null=False)
+#     open = fields.FloatField(null=False)
+#     close = fields.FloatField(null=False)
+#     adjusted_close = fields.FloatField(null=False)
+#     volume = fields.IntField(null=False)
+
+#     stock = fields.ForeignKeyField(
+#         "models.Stock",
+#         related_name="stock_prices",
+#         description="The stock this describe",
+#     )
+
+
+# Stock_Pydantic = pydantic_model_creator(Stock)
+# Stock_Price_Pydantic = pydantic_model_creator(Stock_Price)
+SummarySchema = pydantic_model_creator(TextSummary)
